fb0abb8dab1b5fae23d2b008160a381a472ae284
August Bonds <august.bonds@protonmail.com>
2019-10-19 21:29:28 +0200

    adding auto-incrementing ID to entries

--- TODO.md
+++ TODO.md
@@ -1,4 +1,9 @@
 #TODO
 - store bookmarks to filesystem [x]
-- display list of bookmarks on / []
+- display list of bookmarks on / [x]
 - add bookmark form []
+    - add bookmark endpoint [x]
+- delete bookmark from store []
+    - requires an ID as part of the bookmark data [x]
+    - delete endpoint []
+- login to be able to modify content []
--- app/bookmark_store.py
+++ app/bookmark_store.py
@@ -2,37 +2,43 @@ import configparser
 import json
 
 class BookmarkStore:
-    
+   
     def __init__(self):
+        self.read_conf()
+        print(f"[BookmarkStore] Loaded bookmark store with path {self.db_path}")
+
+    def read_conf(self):
         config = configparser.ConfigParser()
         config.read('conf/app.conf')
         try:
             self.db_path = config['store']['DbPath']
         except KeyError:
-            raise SystemExit(f"[BookmarkStore] Couldn't find DbPath in app.conf, exiting")
-        print(f"[BookmarkStore] Loaded bookmark store with path {self.db_path}")
+           raise SystemExit(f"[BookmarkStore] Couldn't find DbPath in app.conf, exiting") 
     
     def add_bookmark(self, url, title, description):
         store = self.read_db_file()
-        store.append({"url": url, "title": title, "description": description})
+        bookmark = {"id": store['next_id'], "url": url, "title": title, "description": description}
+        store["bookmarks"].append(bookmark)
+        store["next_id"] += 1
         self.write_db_file(store)
-        print(f"[BookmarkStore] Added entry: url={url} title={title} description={description}")
+        print(f"[BookmarkStore] Added entry: id={bookmark['id']} url={url} title={title} description={description}")
 
     def get_bookmarks(self):
         store = self.read_db_file()
-        print(f"[BookmarkStore] Returning all {len(store)} bookmarks")
-        return store
+        bookmarks = store["bookmarks"]
+        print(f"[BookmarkStore] Returning all {len(bookmarks)} bookmarks")
+        return bookmarks
     
-    def write_db_file(self,bookmarks):
+    def write_db_file(self,store):
         with open(self.db_path, 'w') as db:
-            json.dump(bookmarks, db)
+            json.dump(store, db)
     
     def read_db_file(self):
         try:
             with open(self.db_path) as db:
-                bookmarks = json.load(db)
-                return bookmarks
+                store = json.load(db)
+                return store
         except IOError:
             print(f"[BookmarkStore] Db file didn't exist. Will be created by next add operation.")
-            return []
+            return {"next_id": 1, "bookmarks": []}
 
