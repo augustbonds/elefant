6e9b0fdc66807824d2c223b28013069a3daf2a09
August Bonds <august.bonds@protonmail.com>
2019-10-25 22:33:56 +0200

    sort by time and pagination

--- app/bookmark_store.py
+++ app/bookmark_store.py
@@ -1,8 +1,10 @@
 import configparser
 import json
+import datetime
+from collections import defaultdict
 
 class BookmarkStore:
-   
+
     def __init__(self):
         self.read_conf()
         print(f"[BookmarkStore] Loaded bookmark store with path {self.db_path}")
@@ -13,11 +15,11 @@ class BookmarkStore:
         try:
             self.db_path = config['store']['DbPath']
         except KeyError:
-           raise SystemExit(f"[BookmarkStore] Couldn't find DbPath in app.conf, exiting") 
-    
+           raise SystemExit(f"[BookmarkStore] Couldn't find DbPath in app.conf, exiting")
+
     def add_bookmark(self, url, title, description):
         store = self.read_db_file()
-        bookmark = {"id": store['next_id'], "url": url, "title": title, "description": description}
+        bookmark = {"id": store['next_id'],"date_added": str(datetime.datetime.utcnow().isoformat()), "url": url, "title": title, "description": description}
         store["bookmarks"].append(bookmark)
         store["next_id"] += 1
         self.write_db_file(store)
@@ -28,11 +30,16 @@ class BookmarkStore:
         bookmarks = store["bookmarks"]
         print(f"[BookmarkStore] Returning all {len(bookmarks)} bookmarks")
         return bookmarks
-    
+
+    def get_bookmarks_sorted_with_limit(self, offset, limit):
+        bookmarks = self.get_bookmarks()
+        sorted_bookmarks = sorted(bookmarks, key=lambda bookmark: defaultdict(lambda: '', bookmark)["date_added"], reverse=True)
+        return sorted_bookmarks[offset*limit:offset*limit+limit]
+
     def write_db_file(self,store):
         with open(self.db_path, 'w') as db:
             json.dump(store, db)
-    
+
     def read_db_file(self):
         try:
             with open(self.db_path) as db:
--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,11 @@ user = {'username': 'boonspoj'}
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', user=user, bookmarks=store.get_bookmarks())
+    offset = request.args.get('offset', default = 0, type = int)
+    limit = request.args.get('limit', default = 10, type = int)
+    prev_offset = offset if offset == 0 else offset - 1
+    next_offset = offset + 1
+    return render_template('index.html', user=user, bookmarks=store.get_bookmarks_sorted_with_limit(offset,limit), page_num=offset+1, prev_offset=prev_offset, next_offset=next_offset)
 
 @app.route('/bookmarks', methods=['GET'])
 def get_bookmarks():
@@ -21,7 +25,7 @@ def add_bookmark():
     url = request.form['url']
     title = request.form['title']
     description = request.form['description']
-    store.add_bookmark(url=url, title=title, description=description) 
+    store.add_bookmark(url=url, title=title, description=description)
     return redirect('/')
 
 @app.route('/bookmarks/new', methods=['GET', 'POST'])
@@ -30,15 +34,9 @@ def create_new_bookmark():
         url = request.form['url']
         title = request.form['title']
         description = request.form['description']
-        store.add_bookmark(url=url, title=title, description=description) 
+        store.add_bookmark(url=url, title=title, description=description)
         return redirect('/')
     else:
         form = NewBookmarkForm()
         return render_template('createnew.html', form=form, user=user)
-
-    
-        
-
-
-
 
